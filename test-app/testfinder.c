4146826e1a138e3356e653a3f79bf37c3cfdc754
Brandon-T <JustBrandonT@gmail.com>
2014-05-24 19:47:54 -0400

    Fixed finder example

--- test-app/testfinder.c
+++ test-app/testfinder.c
@@ -3,66 +3,6 @@
 #include "bitmap.h"
 #include "finder.h"
 
-
-
-bool findImageToleranceIn2(CTSInfo *info, bitmap *imageToFind, int32_t *x, int32_t *y, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint16_t tolerance)
-{
-    int I, J, XX, YY;
-    info->tol = tolerance;
-    int dX = (x2 - x1) - (imageToFind->width - 1);
-    int dY = (y2 - y1) - (imageToFind->height - 1);
-
-    for (I = 0; I < dY; ++I)
-    {
-        for (J = 0; J < dX; ++J)
-        {
-            for (YY = 0; YY < imageToFind->height; ++YY)
-            {
-                for (XX = 0; XX < imageToFind->width; ++XX)
-                {
-                    rgb32* pixel = &imageToFind->pixels[YY * imageToFind->width + XX];
-                    rgb32* targetPixel = &info->targetImage->pixels[(YY + I) * info->targetImage->width + (XX + J)];
-
-                    if (pixel->a != 0)
-                    {
-                        if (!(*info->ctsFuncPtr)(info, pixel, targetPixel))
-                        {
-                            goto Skip;
-                        }
-                    }
-                }
-            }
-
-            *x = J + x1;
-            *y = I + y1;
-            return true;
-
-            Skip:
-                continue;
-        }
-    }
-
-    *x = -1;
-    *y = -1;
-    return false;
-}
-
-
-bool findImageIn2(CTSInfo *info, bitmap *imageToFind, int32_t *x, int32_t *y, int32_t x1, int32_t y1, int32_t x2, int32_t y2)
-{
-    uint16_t temp = info->CTSNum;
-    setCTS(info, -1);
-    bool Res = findImageToleranceIn2(info, imageToFind, x, y, x1, y1, x2, y2, 0);
-    setCTS(info, temp);
-    return Res;
-}
-
-bool findImage2(CTSInfo *info, bitmap *imageToFind, int32_t *x, int32_t *y)
-{
-    return findImageIn2(info, imageToFind, x, y, 0, 0, info->targetImage->width, info->targetImage->height);
-}
-
-
 void TestOne(CTSInfo* info)
 {
     rgb32 colour = {144, 240, 255, 0};
@@ -84,7 +24,7 @@ void TestTwo(CTSInfo* info, bitmap* bmp_to_find)
 {
     int x, y;
 
-    if (findImageToleranceIn2(info, bmp_to_find, &x, &y, 0, 0, info->targetImage->width, info->targetImage->height, 0))
+    if (findImageToleranceIn(info, bmp_to_find, &x, &y, 0, 0, info->targetImage->width, info->targetImage->height, 0))
     {
         printf("Image found at: (%d, %d)\n", x, y);
     }
